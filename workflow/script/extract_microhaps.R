65b3015e2f97a7750132883a6cf569286f092d3a
Steve Mussmann <17727337+stevemussmann@users.noreply.github.com>
2025-10-06 10:03:58 -0700

    Update extract_microhaps.R

--- workflow/script/extract_microhaps.R
+++ workflow/script/extract_microhaps.R
@@ -13,8 +13,8 @@ sink(log, type = "message")
 
 
 
-sams <- snakemake@input$sams
-sams_dir <- dirname(sams[1])
+bams <- snakemake@input$bams
+bams_dir <- dirname(bams[1])
 
 vcf <- snakemake@input$input_vcf
 
@@ -27,19 +27,19 @@ num_threads <- snakemake@threads[1]
 library(tidyverse)
 library(microhaplotextract)
 
-# get the short file names for the sams, in a tibble
-sams_tibble <- tibble(
-  sams = basename(sams),
-  sample = str_replace(sams, "\\.sam", "")
+# get the short file names for the bams, in a tibble
+bams_tibble <- tibble(
+  bams = basename(bams),
+  sample = str_replace(bams, "\\.bam", "")
 )
 
 # first create the label file by parsing the units.csv
 # joining NMFS_DNA_ID on there and then writing it out
-# into the sams directory
-run_dir_with_species <- str_replace(sams_dir, "/sams.*$", "")
+# into the bams directory
+run_dir_with_species <- str_replace(bams_dir, "/bams.*$", "")
 run_dir <- dirname(run_dir_with_species)
 marker_set <- str_replace(basename(outfile), "--.*$", "")
-label_file_path <- file.path(sams_dir, "labels.txt")
+label_file_path <- file.path(bams_dir, "labels.txt")
 app_path <- file.path(run_dir_with_species, "microhaplot")
 out_path <- tempfile()
 dir.create(out_path, recursive = TRUE, showWarnings = FALSE)
@@ -52,7 +52,7 @@ samples <- read_csv(file.path(run_dir, "samples.csv")) %>%
 
 labels_tibble <- left_join(units, samples, by = "sample") %>%
   select(sample, NMFS_DNA_ID, Sample_Project) %>%
-  mutate(sample = paste0(sample, ".sam"))
+  mutate(sample = paste0(sample, ".bam"))
 
 write.table(
   labels_tibble,
@@ -71,7 +71,7 @@ run_label = basename(outfile) %>%
 
 haplo.read.tbl <- prepHaplotFiles(
   run.label = run_label,
-  sam.path = normalizePath(sams_dir),
+  bam.path = normalizePath(bams_dir),
   out.path = out_path,
   label.path = label_file_path,
   vcf.path = vcf,
